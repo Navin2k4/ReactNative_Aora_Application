7180491fdaecd1f66ebf0e001d8e931880e774a0
Navin Kumaran <navinkumaran2004@gmail.com>
2024-05-11 13:30:42 +0530

    Created the Rect native appliction and configured the Native Wind That uses the tailwing for designing the application

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { Link } from 'expo-router';
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-slate-100">
-            <Text className='text-3xl' >Aora</Text>
+            <Text>Aora</Text>
             <StatusBar style="auto" />
             <Link href="/profile" style={{ color: "blue" }}>Go to Profile</Link>
         </View>
